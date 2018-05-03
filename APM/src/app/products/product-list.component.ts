73dde366ecdcdd3855d1b2f8a7de53f60017e4b1
Maunder <oliver.maunder@dnvgl.com>
2018-05-03 11:03:44 +0100

    Nested components

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -57,4 +57,8 @@ export class ProductListComponent {
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
+
+    onRatingClicked(message: string) : void {
+        this.pageTitle = 'Product List: ' + message;
+    }
 }
